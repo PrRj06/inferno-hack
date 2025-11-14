86ee8bac36fbe13448306ba57f8a6bde293f4689
Prabhat <prabhat.rj06@gmail.com>
2025-11-15 01:55:47 +0530

    added required folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 
 function App() {
   return (
     <>
-      
+      <h1 className='text-red-500'>Hello</h1>
     </>
   )
 }
